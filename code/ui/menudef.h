4d05648d61c52717770bbf45f5bbb8cd0230a4ac
DT <tempust85@hotmail.com>
2018-03-24 17:47:57 +1000

    Added datapad version of ITEM_TYPE_TEXTSCROLL
    - does the exact same thing as ITEM_TYPE_TEXTSCROLL, just with green
    textures.

--- code/ui/menudef.h
+++ code/ui/menudef.h
@@ -36,6 +36,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #define ITEM_TYPE_MULTI				13		// multiple list setting, enumerated
 #define ITEM_TYPE_BIND				14		// multiple list setting, enumerated
 #define ITEM_TYPE_TEXTSCROLL		15		// scrolling text
+#define ITEM_TYPE_TEXTSCROLL_DP		16		// datapad scrolling text 
 
 
 #define ITEM_ALIGN_LEFT 0                 // left alignment
