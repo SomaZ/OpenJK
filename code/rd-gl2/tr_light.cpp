706141b657001899879190d7fe783485ae2e383d
SomaZ <max@veborange.de>
2018-05-02 20:54:47 +0200

    fix over lighting of models with dlights

--- code/rd-gl2/tr_light.cpp
+++ code/rd-gl2/tr_light.cpp
@@ -412,7 +412,7 @@ void R_SetupEntityLighting( const trRefdef_t *refdef, trRefEntity_t *ent ) {
 			factor *= factor;
 
 			d = factor / d;
-			d *= DLIGHT_AT_RADIUS * dl->radius;
+			d *= dl->radius;
 		}
 		else 
 		{
