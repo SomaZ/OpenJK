2c280cb286370286190fb90af0d93be96c74571f
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-01-24 00:27:48 +0100

    [rend2] Add alpha test that discards everything except full opacity
    Also reordering some glsl defines -> no change in functionality

--- shared/rd-rend2/glsl/lightall.glsl
+++ shared/rd-rend2/glsl/lightall.glsl
@@ -1029,6 +1029,11 @@ void main()
 		if (diffuse.a < 0.75)
 			discard;
 	}
+	else if (u_AlphaTestType == ALPHA_TEST_E255)
+	{
+		if (diffuse.a < 1.00)
+			discard;
+	}
 #endif
 
 #if defined(PER_PIXEL_LIGHTING)
