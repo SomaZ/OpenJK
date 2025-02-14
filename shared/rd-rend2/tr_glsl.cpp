fc7b58679164a80c571b0e6eacc400d4d0d4ac09
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-02-14 23:13:03 +0100

    [rend2] Implement SMAA T2x
    Velocity calculations can be simplified as currently all surfaces need to be evaluated individually even if we could compute the velocity just by depth and the view projection matrices for all static surfaces.

--- shared/rd-rend2/tr_glsl.cpp
+++ shared/rd-rend2/tr_glsl.cpp
@@ -32,8 +32,11 @@ const uniformBlockInfo_t uniformBlocksInfo[UNIFORM_BLOCK_COUNT] = {
 	{ 2, "Lights", sizeof(LightsBlock) },
 	{ 3, "Fogs", sizeof(FogsBlock) },
 	{ 4, "Entity", sizeof(EntityBlock) },
-	{ 5, "ShaderInstance", sizeof(ShaderInstanceBlock) },
-	{ 6, "Bones", sizeof(SkeletonBoneMatricesBlock) },
+	{ 5, "PreviousEntity", sizeof(EntityBlock) },
+	{ 6, "ShaderInstance", sizeof(ShaderInstanceBlock) },
+	{ 7, "Bones", sizeof(SkeletonBoneMatricesBlock) },
+	{ 8, "PreviousBones", sizeof(SkeletonBoneMatricesBlock) },
+	{ 9, "TemporalInfo", sizeof(TemporalBlock) },
 	{ 10, "SurfaceSprite", sizeof(SurfaceSpriteBlock) },
 };
 
@@ -67,6 +70,7 @@ static uniformInfo_t uniformsInfo[] =
 	{ "u_AreaMap", GLSL_INT, 1 },
 	{ "u_SearchMap", GLSL_INT, 1 },
 	{ "u_BlendMap", GLSL_INT, 1 },
+	{ "u_VelocityMap", GLSL_INT, 1 },
 
 	{ "u_ShadowMap",  GLSL_INT, 1 },
 	{ "u_ShadowMap2", GLSL_INT, 1 },
@@ -1568,6 +1572,79 @@ static int GLSL_LoadGPUProgramFogPass(
 	return numPrograms;
 }
 
+static int GLSL_LoadGPUProgramVelocityPass(
+	ShaderProgramBuilder& builder,
+	Allocator& scratchAlloc)
+{
+	int numPrograms = 0;
+	Allocator allocator(scratchAlloc.Base(), scratchAlloc.GetSize());
+
+	char extradefines[1200];
+	const GPUProgramDesc *programDesc =
+		LoadProgramSource("velocity", allocator, fallback_velocityProgram);
+	for (int i = 0; i < VELOCITYDEF_COUNT; i++)
+	{
+		if (!GLSL_IsValidPermutationForFog(i))
+		{
+			continue;
+		}
+
+		uint32_t attribs =
+			(ATTR_POSITION | ATTR_NORMAL | ATTR_TEXCOORD0);
+		extradefines[0] = '\0';
+
+		if (i & VELOCITYDEF_USE_DEFORM_VERTEXES)
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_DEFORM_VERTEXES\n");
+#ifdef REND2_SP_MD3
+		if (i & FOGDEF_USE_VERTEX_ANIMATION)
+		{
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_VERTEX_ANIMATION\n");
+			attribs |= ATTR_POSITION2 | ATTR_NORMAL2;
+		}
+#endif // REND2_SP
+		if (i & VELOCITYDEF_USE_SKELETAL_ANIMATION)
+		{
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_SKELETAL_ANIMATION\n");
+			attribs |= ATTR_BONE_INDEXES | ATTR_BONE_WEIGHTS;
+		}
+
+		if (i & VELOCITYDEF_USE_TCGEN_AND_TCMOD)
+		{
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_TCGEN\n");
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_TCMOD\n");
+		}
+
+		if (i & VELOCITYDEF_USE_RGBAGEN)
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_RGBAGEN\n");
+
+		if (i & VELOCITYDEF_USE_PARALLAXMAP && r_parallaxMapping->integer)
+		{
+			Q_strcat(extradefines, sizeof(extradefines), "#define USE_PARALLAXMAP\n");
+			attribs |= ATTR_TANGENT;
+		}
+
+		if (!GLSL_LoadGPUShader(builder, &tr.velocityShader[i], "velocity", attribs, NO_XFB_VARS,
+			extradefines, *programDesc))
+		{
+			ri.Error(ERR_FATAL, "Could not load velocity shader!");
+		}
+
+		GLSL_InitUniforms(&tr.velocityShader[i]);
+
+		qglUseProgram(tr.velocityShader[i].program);
+		//if (i & FOGDEF_USE_ALPHA_TEST)
+		GLSL_SetUniformInt(&tr.velocityShader[i], UNIFORM_DIFFUSEMAP, TB_DIFFUSEMAP);
+		GLSL_SetUniformInt(&tr.velocityShader[i], UNIFORM_NORMALMAP, TB_NORMALMAP);
+		qglUseProgram(0);
+
+		GLSL_FinishGPUShader(&tr.velocityShader[i]);
+
+		++numPrograms;
+	}
+
+	return numPrograms;
+}
+
 static int GLSL_LoadGPUProgramRefraction(
 	ShaderProgramBuilder& builder,
 	Allocator& scratchAlloc)
@@ -2005,7 +2082,7 @@ static int GLSL_LoadGPUProgramTonemap(
 	const uint32_t attribs = ATTR_POSITION | ATTR_TEXCOORD0;
 
 	extradefines[0] = '\0';
-	if (r_smaa->integer)
+	if (r_smaa->integer == 1)
 	{
 		Q_strcat(extradefines, sizeof(extradefines),
 			va( "#define USE_SMAA\n"
@@ -2036,7 +2113,7 @@ static int GLSL_LoadGPUProgramTonemap(
 		qglUseProgram(tr.tonemapShader[i].program);
 		GLSL_SetUniformInt(&tr.tonemapShader[i], UNIFORM_TEXTUREMAP, TB_COLORMAP);
 		GLSL_SetUniformInt(&tr.tonemapShader[i], UNIFORM_LEVELSMAP, TB_LEVELSMAP);
-		if (r_smaa->integer)
+		if (r_smaa->integer == 1)
 			GLSL_SetUniformInt(&tr.tonemapShader[i], UNIFORM_BLENDMAP, 2);
 
 		qglUseProgram(0);
@@ -2305,7 +2382,11 @@ static int GLSL_LoadGPUProgramSurfaceSprites(
 		if (i & SSDEF_ADDITIVE)
 			Q_strcat(extradefines, sizeof(extradefines),
 				"#define ADDITIVE_BLEND\n");
-		
+
+		if (i & SSDEF_VELOCITY)
+			Q_strcat(extradefines, sizeof(extradefines),
+				"#define VELOCITY_PASS\n");
+
 		shaderProgram_t *program = tr.spriteShader + i;
 		if (!GLSL_LoadGPUShader(builder, program, "surface_sprites", attribs, NO_XFB_VARS,
 				extradefines, *programDesc))
@@ -2364,6 +2445,7 @@ static int GLSL_LoadGPUProgramSMAA(
 	
 	Q_strcat(extradefines, sizeof(extradefines),
 		va(	"#define SMAA_LOCAL_CONTRAST_ADAPTATION_FACTOR 2.0\n"
+			"#define SMAA_REPROJECTION_WEIGHT_SCALE 30.0\n"
 			"#define SMAA_RT_METRICS vec4(1.0 / %f, 1.0 / %f, %f, %f)\n",
 			(float)glConfig.vidWidth,
 			(float)glConfig.vidHeight,
@@ -2403,6 +2485,14 @@ static int GLSL_LoadGPUProgramSMAA(
 			"#define SMAA_MAX_SEARCH_STEPS 32\n"
 			"#define SMAA_MAX_SEARCH_STEPS_DIAG 16\n"
 			"#define SMAA_CORNER_ROUNDING 25\n"
+
+		);
+	}
+
+	if (r_smaa->integer == 2)
+	{
+		Q_strcat(extradefines, sizeof(extradefines),
+			"#define SMAA_REPROJECTION 1\n"
 		);
 	}
 	
@@ -2428,8 +2518,8 @@ static int GLSL_LoadGPUProgramSMAA(
 			builder,
 			scratchAlloc,
 			&tr.smaaBlendShader,
-			"smaaBlend",
-			fallback_smaaBlendProgram,
+			"smaaBlendWeight",
+			fallback_smaaBlendWeightProgram,
 			extradefines);
 
 		GLSL_InitUniforms(&tr.smaaBlendShader);
@@ -2442,7 +2532,46 @@ static int GLSL_LoadGPUProgramSMAA(
 
 		GLSL_FinishGPUShader(&tr.smaaBlendShader);
 	}
-	return 2;
+	{
+		GLSL_LoadGPUProgramBasicWithDefinitions(
+			builder,
+			scratchAlloc,
+			&tr.smaaResolveShader,
+			"smaaResolve",
+			fallback_smaaResolveProgram,
+			extradefines);
+
+		GLSL_InitUniforms(&tr.smaaResolveShader);
+
+		qglUseProgram(tr.smaaResolveShader.program);
+		GLSL_SetUniformInt(&tr.smaaResolveShader, UNIFORM_TEXTUREMAP, 0);
+		GLSL_SetUniformInt(&tr.smaaResolveShader, UNIFORM_BLENDMAP, 1);
+		if (r_smaa->integer == 2)
+			GLSL_SetUniformInt(&tr.smaaResolveShader, UNIFORM_VELOCITYMAP, 2);
+		qglUseProgram(0);
+
+		GLSL_FinishGPUShader(&tr.smaaResolveShader);
+	}
+	{
+		GLSL_LoadGPUProgramBasicWithDefinitions(
+			builder,
+			scratchAlloc,
+			&tr.smaaTemporalResolveShader,
+			"smaaTemporalResolve",
+			fallback_smaaTemporalResolveProgram,
+			extradefines);
+
+		GLSL_InitUniforms(&tr.smaaTemporalResolveShader);
+
+		qglUseProgram(tr.smaaTemporalResolveShader.program);
+		GLSL_SetUniformInt(&tr.smaaTemporalResolveShader, UNIFORM_TEXTUREMAP, 0);
+		GLSL_SetUniformInt(&tr.smaaTemporalResolveShader, UNIFORM_BLENDMAP, 1);
+		GLSL_SetUniformInt(&tr.smaaTemporalResolveShader, UNIFORM_VELOCITYMAP, 2);
+		qglUseProgram(0);
+
+		GLSL_FinishGPUShader(&tr.smaaTemporalResolveShader);
+	}
+	return 4;
 }
 
 void GLSL_LoadGPUShaders()
@@ -2514,6 +2643,7 @@ void GLSL_LoadGPUShaders()
 	numGenShaders += GLSL_LoadGPUProgramGeneric(builder, allocator);
 	numLightShaders += GLSL_LoadGPUProgramLightAll(builder, allocator);
 	numEtcShaders += GLSL_LoadGPUProgramFogPass(builder, allocator);
+	numEtcShaders += GLSL_LoadGPUProgramVelocityPass(builder, allocator);
 	numEtcShaders += GLSL_LoadGPUProgramRefraction(builder, allocator);
 	numEtcShaders += GLSL_LoadGPUProgramTextureColor(builder, allocator);
 	numEtcShaders += GLSL_LoadGPUProgramPShadow(builder, allocator);
@@ -2565,6 +2695,9 @@ void GLSL_ShutdownGPUShaders(void)
 	for ( i = 0; i < FOGDEF_COUNT; i++)
 		GLSL_DeleteGPUShader(&tr.fogShader[i]);
 
+	for (i = 0; i < VELOCITYDEF_COUNT; i++)
+		GLSL_DeleteGPUShader(&tr.velocityShader[i]);
+
 	for ( i = 0; i < LIGHTDEF_COUNT; i++)
 		GLSL_DeleteGPUShader(&tr.lightallShader[i]);
 
@@ -2603,6 +2736,8 @@ void GLSL_ShutdownGPUShaders(void)
 
 	GLSL_DeleteGPUShader(&tr.smaaEdgeShader);
 	GLSL_DeleteGPUShader(&tr.smaaBlendShader);
+	GLSL_DeleteGPUShader(&tr.smaaResolveShader);
+	GLSL_DeleteGPUShader(&tr.smaaTemporalResolveShader);
 
 	glState.currentProgram = 0;
 	qglUseProgram(0);
--- shared/rd-rend2/tr_local.h
+++ shared/rd-rend2/tr_local.h
@@ -799,6 +799,15 @@ struct SkeletonBoneMatricesBlock
 	mat3x4_t matrices[MAX_G2_BONES];
 };
 
+struct TemporalBlock
+{
+	matrix_t previousViewProjectionMatrix;
+	vec2_t currentJitter;
+	vec2_t previousJitter;
+	float previousTime;
+	float pad0[3];
+};
+
 struct surfaceSprite_t
 {
 	surfaceSpriteType_t type;
@@ -1186,12 +1195,20 @@ enum
 
 enum
 {
+	TEXCOLORDEF_USE_VERTICES = 0x0000,
+	TEXCOLORDEF_SCREEN_TRIANGLE = 0x0001,
+
+	TEXCOLORDEF_ALL = 0x0001,
+	TEXCOLORDEF_COUNT = TEXCOLORDEF_ALL + 1,
+};
+
+enum
+{
 	GENERICDEF_USE_DEFORM_VERTEXES 		= 0x0001,
 	GENERICDEF_USE_TCGEN_AND_TCMOD 		= 0x0002,
 	GENERICDEF_USE_FOG             		= 0x0004,
 	GENERICDEF_USE_RGBAGEN         		= 0x0008,
 	GENERICDEF_USE_SKELETAL_ANIMATION	= 0x0010,
-	// GENERICDEF_USE_GLOW_BUFFER      	= 0x0020,
 	// GENERICDEF_USE_ALPHA_TEST			= 0x0040,
 #ifdef REND2_SP_MD3
 	GENERICDEF_USE_VERTEX_ANIMATION		= 0x0080,
@@ -1201,15 +1218,6 @@ enum
 #endif // REND2_SP
 	
 	GENERICDEF_COUNT                	= GENERICDEF_ALL + 1,
-};
-
-enum
-{
-	TEXCOLORDEF_USE_VERTICES			= 0x0000,
-	TEXCOLORDEF_SCREEN_TRIANGLE			= 0x0001,
-
-	TEXCOLORDEF_ALL						= 0x0001,
-	TEXCOLORDEF_COUNT					= TEXCOLORDEF_ALL + 1,
 };
 
 enum
@@ -1226,6 +1234,23 @@ enum
 #endif // REND2_SP
 
 	FOGDEF_COUNT                		= FOGDEF_ALL + 1,
+};
+
+enum
+{
+	VELOCITYDEF_USE_DEFORM_VERTEXES		= 0x0001,
+	VELOCITYDEF_USE_SKELETAL_ANIMATION	= 0x0002,
+	VELOCITYDEF_USE_TCGEN_AND_TCMOD		= 0x0004,
+	VELOCITYDEF_USE_RGBAGEN				= 0x0008,
+	VELOCITYDEF_USE_PARALLAXMAP			= 0x0010,
+	//VELOCITYDEF_USE_ALPHA_TEST		= 0x0004,
+#ifdef REND2_SP_MD3
+	VELOCITYDEF_USE_VERTEX_ANIMATION	= 0x0004,
+	VELOCITYDEF_ALL						= 0x0007,
+#else
+	VELOCITYDEF_ALL						= 0x001F,
+#endif // REND2_SP
+	VELOCITYDEF_COUNT					= VELOCITYDEF_ALL + 1,
 };
 
 enum
@@ -1253,9 +1278,7 @@ enum
 	LIGHTDEF_USE_LIGHT_VERTEX    		= 0x0003,
 	LIGHTDEF_USE_TCGEN_AND_TCMOD 		= 0x0004,
 	LIGHTDEF_USE_PARALLAXMAP     		= 0x0008,
-//	LIGHTDEF_USE_SHADOWMAP       		= 0x0010,
 	LIGHTDEF_USE_SKELETAL_ANIMATION 	= 0x0010,
-	//LIGHTDEF_USE_GLOW_BUFFER     		= 0x0020,
 	//LIGHTDEF_USE_ALPHA_TEST		 		= 0x0040,
 	LIGHTDEF_USE_CLOTH_BRDF				= 0x0020,
 	LIGHTDEF_USE_SPEC_GLOSS				= 0x0040,
@@ -1284,8 +1307,9 @@ enum
 	SSDEF_FOG_MODULATE					= 0x10,
 	SSDEF_ADDITIVE						= 0x20,
 	SSDEF_FLATTENED						= 0x40,
+	SSDEF_VELOCITY						= 0x80,
 
-	SSDEF_ALL							= 0x7F,
+	SSDEF_ALL							= 0xFF,
 	SSDEF_COUNT							= SSDEF_ALL + 1
 };
 
@@ -1307,8 +1331,11 @@ enum uniformBlock_t
 	UNIFORM_BLOCK_LIGHTS,
 	UNIFORM_BLOCK_FOGS,
 	UNIFORM_BLOCK_ENTITY,
+	UNIFORM_BLOCK_PREVIOUS_ENTITY,
 	UNIFORM_BLOCK_SHADER_INSTANCE,
 	UNIFORM_BLOCK_BONES,
+	UNIFORM_BLOCK_PREVIOUS_BONES,
+	UNIFORM_BLOCK_TEMPORAL_INFO,
 	UNIFORM_BLOCK_SURFACESPRITE,
 	UNIFORM_BLOCK_COUNT
 };
@@ -1378,6 +1405,7 @@ typedef enum
 	UNIFORM_AREAMAP,
 	UNIFORM_SEARCHMAP,
 	UNIFORM_BLENDMAP,
+	UNIFORM_VELOCITYMAP,
 
 	UNIFORM_SHADOWMAP,
 	UNIFORM_SHADOWMAP2,
@@ -2457,6 +2485,9 @@ typedef struct trGlobals_s {
 
 	image_t					*renderImage;
 	image_t					*glowImage;
+	image_t					*velocityImage;
+	image_t					*temporalResolveImage;
+	image_t					*historyImage;
 	image_t					*glowImageScaled[6];
 	image_t					*sunRaysImage;
 	image_t					*renderDepthImage;
@@ -2479,10 +2510,13 @@ typedef struct trGlobals_s {
 	image_t					*smaaAreaImage;
 	image_t					*smaaEdgeImage;
 	image_t					*smaaBlendImage;
+	image_t					*smaaResolveImage;
 
 	FBO_t					*renderFbo;
+	FBO_t					*depthVelocityFbo;
 	FBO_t					*glowFboScaled[6];
 	FBO_t					*msaaResolveFbo;
+	FBO_t					*msaaResolveVelocityFbo;
 	FBO_t					*sunRaysFbo;
 	FBO_t					*depthFbo;
 	FBO_t					*pshadowFbos[MAX_DRAWN_PSHADOWS];
@@ -2499,6 +2533,9 @@ typedef struct trGlobals_s {
 	FBO_t					*weatherDepthFbo;
 	FBO_t					*smaaEdgeFbo;
 	FBO_t					*smaaBlendFbo;
+	FBO_t					*smaaResolveFbo;
+	FBO_t					*temporalResolveFbo;
+	FBO_t					*historyFbo;
 
 	shader_t				*defaultShader;
 	shader_t				*shadowShader;
@@ -2536,6 +2573,7 @@ typedef struct trGlobals_s {
 	shaderProgram_t refractionShader[REFRACTIONDEF_COUNT];
 	shaderProgram_t textureColorShader[TEXCOLORDEF_COUNT];
 	shaderProgram_t fogShader[FOGDEF_COUNT];
+	shaderProgram_t velocityShader[VELOCITYDEF_COUNT];
 	shaderProgram_t lightallShader[LIGHTDEF_COUNT];
 	shaderProgram_t pshadowShader;
 	shaderProgram_t volumeShadowShader;
@@ -2558,6 +2596,7 @@ typedef struct trGlobals_s {
 	shaderProgram_t smaaEdgeShader;
 	shaderProgram_t smaaBlendShader;
 	shaderProgram_t smaaResolveShader;
+	shaderProgram_t smaaTemporalResolveShader;
 
 	GLuint staticUbo;
 	GLuint spriteUbos[MAX_SUB_BSP + 1];
@@ -2574,11 +2613,14 @@ typedef struct trGlobals_s {
 
 	int cameraUboOffsets[3 + MAX_DLIGHTS * 6 + 3 + MAX_DRAWN_PSHADOWS];
 	int sceneUboOffset;
+	int temporalInfoUboOffset;
 	int lightsUboOffset;
 	int fogsUboOffset;
 	int skyEntityUboOffset;
 	int entityUboOffsets[REFENTITYNUM_WORLD + 1];
+	int previousEntityUboOffsets[REFENTITYNUM_WORLD + 1];
 	int animationBoneUboOffset;
+	int previousAnimationBoneUboOffset;
 
 	// -----------------------------------------
 
@@ -3459,6 +3501,7 @@ void R_AddGhoulSurfaces( trRefEntity_t *ent, int entityNum );
 void RB_SurfaceGhoul( CRenderableSurface *surf );
 void RB_TransformBones(const trRefEntity_t *ent, const trRefdef_t *refdef, int currentFrameNum, gpuFrame_t *frame);
 int RB_GetBoneUboOffset(CRenderableSurface *surf);
+int RB_GetPreviousBoneUboOffset(CRenderableSurface *surf);
 void RB_SetBoneUboOffset(CRenderableSurface *surf, int offset, int currentFrameNum);
 void RB_FillBoneBlock(CRenderableSurface *surf, mat3x4_t *outMatrices);
 /*
@@ -3668,6 +3711,24 @@ struct screenshotReadback_t
 	char filename[MAX_QPATH];
 };
 
+struct ghoul2UboCache_t
+{
+	void *ghoulPointer;
+	qhandle_t model;
+	int boltIndex;
+	int boneUboOffset;
+};
+
+struct modelUboCache_t
+{
+	modtype_t type;
+	qhandle_t hModel;
+	void      *ghoulPointer;
+	vec3_t    origin;
+	vec3_t    velocity;
+	int       modelUboOffset;
+};
+
 #define MAX_GPU_TIMERS (512)
 #define MAX_SCENES (3)
 struct gpuFrame_t
@@ -3695,6 +3756,15 @@ struct gpuFrame_t
 	int numTimers;
 	int numTimedBlocks;
 
+	int numCachedGhoulUboOffsets;
+	ghoul2UboCache_t cachedGhoulUboOffsets[MAX_GENTITIES];
+
+	int numCachedModelUboOffsets;
+	modelUboCache_t cachedModelUboOffsets[MAX_REFENTITIES];
+
+	matrix_t viewProjectionMatrix;
+	float    time;
+
 	gpuTimer_t timers[MAX_GPU_TIMERS];
 	gpuTimedBlock_t timedBlocks[MAX_GPU_TIMERS / 2]; // Each block will need 2 timer queries.
 };
@@ -3709,8 +3779,13 @@ typedef struct backEndData_s {
 	unsigned realFrameNumber;
 	gpuFrame_t frames[MAX_FRAMES];
 	gpuFrame_t *currentFrame;
+	gpuFrame_t *previousFrame;
 	Allocator *perFrameMemory;
 	Pass *currentPass;
+
+	bool cachePreviousFrameUbos;
+	uint32_t numFrameUbos;
+	GLuint *frameUbos;
 
 	drawSurf_t	drawSurfs[MAX_DRAWSURFS];
 	dlight_t	dlights[MAX_DLIGHTS];
