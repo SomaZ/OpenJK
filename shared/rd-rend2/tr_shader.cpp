4703f4190749c7780d4242b9917332020a18c796
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-02-14 20:03:44 +0100

    [rend2] Make depthFunc equal stages not write depth
    Helps determain if a stage will probably write depth values or not. If the depthfunc is equal, you wont write new or different depth values so just disable it.

--- shared/rd-rend2/tr_shader.cpp
+++ shared/rd-rend2/tr_shader.cpp
@@ -1537,6 +1537,7 @@ static qboolean ParseStage( shaderStage_t *stage, const char **text )
 			else if ( !Q_stricmp( token, "equal" ) )
 			{
 				depthFuncBits = GLS_DEPTHFUNC_EQUAL;
+				depthMaskBits = 0;
 			}
 			else if ( !Q_stricmp( token, "disable" ) )
 			{
