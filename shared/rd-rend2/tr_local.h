d1f08ad3fbaa3c8c206264cbcaadcd35f1190738
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-02-25 12:28:56 +0100

    [rend2] Sort sky draws to prevent popping
    When multiple skys are rendered per frame, they could pop randomly because of inconsitent sorting. Also makes sure sky stages are rendered after the actual sky image.

--- shared/rd-rend2/tr_local.h
+++ shared/rd-rend2/tr_local.h
@@ -2446,6 +2446,7 @@ typedef struct {
 	backEndCounters_t	pc;
 	trRefEntity_t	*currentEntity;
 	qboolean	skyRenderedThisView;	// flag for drawing sun
+	byte		skyNumber;
 
 	qboolean	projection2D;	// if qtrue, drawstretchpic doesn't need to change modes
 	float		color2D[4];
