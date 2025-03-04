11afedd810da0c78e2294d612801e70fbf2e99c1
SomaZ <17459161+SomaZ@users.noreply.github.com>
2025-03-04 21:14:26 +0100

    [rend2] Skip drawing cg_shadows 4 when not initialized
    Prevents drawing a giant blob shadow beneath players when cg_shadows 4 is set after loading a map without it being active.

--- shared/rd-rend2/tr_shade.cpp
+++ shared/rd-rend2/tr_shade.cpp
@@ -2116,9 +2116,10 @@ void RB_StageIteratorGeneric( void )
 		// pshadows!
 		//
 		if (r_shadows->integer == 4 &&
-				tess.pshadowBits &&
-				tess.shader->sort <= SS_OPAQUE &&
-				!(tess.shader->surfaceFlags & (SURF_NODLIGHT | SURF_SKY)))
+			tess.pshadowBits &&
+			tess.shader->sort <= SS_OPAQUE &&
+			!(tess.shader->surfaceFlags & (SURF_NODLIGHT | SURF_SKY)) &&
+			tr.pshadowArrayImage)
 		{
 			ProjectPshadowVBOGLSL( input, &vertexArrays );
 		}
