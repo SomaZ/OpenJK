9b9c65fd7fc4b437317218f84fbc35093f044c10
SomaZ <17459161+SomaZ@users.noreply.github.com>
2026-03-13 20:49:33 +0100

    [rend2] Fix surface sprite shader stage overflow crash
    with lightstyles and/or fog stages. Surfaces counted the number of surface sprites on shader read. The system might add light stages based on lightstyles and/or fog and might skip the last few stages depending on added stages. Need to check if there's actually memory leaking in this case or not. If so, vanilla is affected as well.

--- shared/rd-rend2/tr_bsp.cpp
+++ shared/rd-rend2/tr_bsp.cpp
@@ -4142,7 +4142,6 @@ static void R_GenerateSurfaceSprites( const world_t *world, int worldIndex )
 					if (j > 0 && (stage->stateBits & GLS_DEPTHFUNC_EQUAL))
 					{
 						ri.Printf(PRINT_WARNING, "depthFunc equal is not supported on surface sprites in rend2. Skipping stage\n");
-						surf->numSurfaceSprites -= 1;
 						continue;
 					}
 
@@ -4172,6 +4171,8 @@ static void R_GenerateSurfaceSprites( const world_t *world, int worldIndex )
 
 					++surfaceSpriteNum;
 				}
+				surf->numSurfaceSprites = surfaceSpriteNum;
+
 				break;
 			}
 
