c38bceb0e012126464e2941b3284fc1342b96d3f
TwelveEyes <softhaxorz@gmail.com>
2026-06-29 12:19:40 -0700

    [rend2] Fix gore buffer incorrectly using `glBufferSubData` when immutable (#67)
    * [rend2] Fix gore buffer incorrectly using `glBufferSubData` when immutable
    
    * [rend2] Unmap the gore buffer on shutdown when immutable
    
    * [rend2] Missing preprocessor guards

--- shared/rd-rend2/tr_local.h
+++ shared/rd-rend2/tr_local.h
@@ -3841,8 +3841,10 @@ struct gpuFrame_t
 
 #ifdef _G2_GORE
 	VBO_t					*goreVBO;
+	void					*goreVBOMemory;
 	int						goreVBOCurrentIndex;
 	IBO_t					*goreIBO;
+	void					*goreIBOMemory;
 	int						goreIBOCurrentIndex;
 #endif
 
