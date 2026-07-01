ced9b1f937fdb71ff497f685d51dd82a482dbc0e
SomaZ <17459161+SomaZ@users.noreply.github.com>
2026-07-02 01:18:01 +0200

    [rend2] Fix dynamic lights on md3 surfaces

--- shared/rd-rend2/tr_backend.cpp
+++ shared/rd-rend2/tr_backend.cpp
@@ -1396,7 +1396,6 @@ static void RB_SubmitDrawSurfs(
 {
 	shader_t *oldShader = nullptr;
 	int64_t oldEntityNum = -1;
-	int64_t oldSort = -1;
 	int64_t oldFogNum = -1;
 	int64_t oldDlighted = 0;
 	int64_t oldPostRender = 0;
@@ -1424,6 +1423,7 @@ static void RB_SubmitDrawSurfs(
 			{
 				RB_EndSurface();
 				RB_BeginSurface(shader, fogNum, cubemapIndex);
+				tess.dlightBits = dlighted;
 				oldBoneCache = ((CRenderableSurface*)drawSurf->surface)->boneCache;
 				tr.animationBoneUboOffset = RB_GetBoneUboOffset((CRenderableSurface*)drawSurf->surface);
 				tr.previousAnimationBoneUboOffset = RB_GetPreviousBoneUboOffset((CRenderableSurface*)drawSurf->surface);
@@ -1442,8 +1442,6 @@ static void RB_SubmitDrawSurfs(
 			rb_surfaceTable[*drawSurf->surface](drawSurf->surface);
 			continue;
 		}
-
-		oldSort = drawSurf->sort;
 
 		//
 		// change the tess parameters if needed
@@ -1468,6 +1466,7 @@ static void RB_SubmitDrawSurfs(
 			oldDlighted = dlighted;
 			oldPostRender = postRender;
 			oldCubemapIndex = cubemapIndex;
+			tess.dlightBits = dlighted;
 		}
 
 		if ( entityNum != oldEntityNum )
@@ -1481,10 +1480,6 @@ static void RB_SubmitDrawSurfs(
 
 		if (backEnd.refractionFill != isDistortionShader)
 			continue;
-
-		// ugly hack for now...
-		// find better way to pass dlightbits
-		tess.dlightBits = drawSurf->dlightBits;
 
 		// add the triangles for this surface
 		rb_surfaceTable[*drawSurf->surface](drawSurf->surface);
